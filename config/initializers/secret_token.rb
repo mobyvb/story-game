d3551685e9b60b1344accbc877b3abd347c494d9
mobyvb <mobyvb@gmail.com>
2013-07-06 23:58:51 -0400

    initial commit (forreal)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StoryGame::Application.config.secret_key_base = '228c16d544e70941c9beb04db613f6e87702ad8c8a5792bc9a2f91bba9440fffe6bf000c3032a66909012bcdbe8d4cf5a19edbf2be88c496ea451b5f2eb72313'
