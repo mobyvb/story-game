7a3a075002685b38882fedd8860b70481220c50e
mobyvb <mobyvb@gmail.com>
2013-11-17 22:58:25 -0500

    design profile page

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -67,28 +67,28 @@ exports.signin = function(req, res) {
       user.comparePassword(req.body.password, function(err, isMatch) {
         if(err) {
           console.log(err);
-          res.redirect('/');
+          res.redirect('/profile');
         }
         else if(isMatch) {
           req.session.username = user.username;
-          res.redirect('/');
+          res.redirect('/profile');
         }
         else {
           req.session.errors = {signin:['incorrect username or password']};
-          res.redirect('/');
+          res.redirect('/profile');
         }
       });
     }
     else {
       req.session.errors = {signin:[req.body.username + ' doesn\'t exist']};
-      res.redirect('/');
+      res.redirect('/profile');
     }
   });
 };
 
 exports.signout = function(req, res) {
   req.session.username = undefined;
-  res.redirect('/');
+  res.redirect('/profile');
 };
 
 exports.addFriend = function(req, res) {
@@ -96,7 +96,7 @@ exports.addFriend = function(req, res) {
   var friendName = req.body.username.replace(' ', '');
   if(friendName === currUserName) {
     req.session.errors = {friends:['you can\'t be friends with yourself']};
-    res.redirect('/');
+    res.redirect('/profile');
   }
   else {
     User.findOne({username:friendName}, function(err, friend) {
@@ -110,25 +110,25 @@ exports.addFriend = function(req, res) {
               currUser.save();
               friend.pendingFriends.push(currUserName);
               friend.save();
-              res.redirect('/');
+              res.redirect('/profile');
             }
             else if(currUser.pendingFriends.indexOf(friendName) !== -1) {
               var index = currUser.pendingFriends.indexOf(friendName);
               currUser.friends.push(friendName);
               currUser.pendingFriends.splice(index, 1);
               currUser.save();
-              res.redirect('/');
+              res.redirect('/profile');
             }
             else {
               req.session.errors = {friends:['already friends with ' + friendName]};
-              res.redirect('/');
+              res.redirect('/profile');
             }
           }
         });
       }
       else {
         req.session.errors = {friends:['there is no user named ' + friendName]};
-        res.redirect('/');
+        res.redirect('/profile');
       }
     });
   }
@@ -164,16 +164,16 @@ exports.addEmail = function(req, res) {
             }
           });
 
-          res.redirect('/');
+          res.redirect('/profile');
         }
         else {
-          res.redirect('/');
+          res.redirect('/profile');
         }
       });
     }
     else {
       req.session.errors = {email:['please enter a valid email']};
-      res.redirect('/');
+      res.redirect('/profile');
     }
   }
   else if(req.params.code) {
@@ -182,14 +182,14 @@ exports.addEmail = function(req, res) {
         console.log(err);
       }
       else if(!user) {
-        res.redirect('/');
+        res.redirect('/profile');
       }
       else {
         user.email = user.pendingEmail;
         user.emailVerify = '';
         user.pendingEmail = '';
         user.save();
-        res.redirect('/');
+        res.redirect('/profile');
       }
     });
   }
@@ -212,20 +212,20 @@ exports.addPhone = function(req, res) {
             body: 'You are receiving notifications for the story game.'
           });
 
-          res.redirect('/');
+          res.redirect('/profile');
         }
         else {
-          res.redirect('/');
+          res.redirect('/profile');
         }
       });
     }
     else {
       req.session.errors = {phone:['please enter a valid US phone number']};
-      res.redirect('/');
+      res.redirect('/profile');
     }
   }
   else {
     req.session.errors = {phone:['please enter a valid phone number']};
-    res.redirect('/');
+    res.redirect('/profile');
   }
 }
